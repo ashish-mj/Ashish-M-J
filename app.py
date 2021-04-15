72b2378b59d90ddb75730d3ef98dc7fd7bebeb33
Ashish M J <ashm.jagadeesh@gmail.com>
2021-04-16 00:52:17 +0530

    Create About Page

--- app.py
+++ app.py
@@ -29,6 +29,12 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
 @app.route('/contact',methods=['GET','POST'])
 def contact():
     if request.method=='POST':
