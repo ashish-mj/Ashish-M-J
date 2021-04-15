26e3f74a0e8a8e2f35a7301f220a22ba0345aece
Ashish M J <ashm.jagadeesh@gmail.com>
2021-04-15 18:18:24 +0530

    Create Home Page

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['MAIL_USE_SSL']=True
 
 @app.route('/')
 def home():
-    return render_template('template.html')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
